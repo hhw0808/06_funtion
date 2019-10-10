1bb18042001953ac50b1bb39c21e8572c7fc9a25
rkdms0327 <55428479+rkdms0327@users.noreply.github.com>
2019-10-10 15:29:01 +0900

    01

---
+++ main.c
@@ -0,0 +1,23 @@
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+#include <stdio.h>
+
+void print_star()
+{
+	int i;
+	for(i=0; i<10; i++)
+	 printf("*");
+} 
+  
+
+int main(void)
+{
+	int i;
+	 
+	print_star();
+	print_star();
+	print_star();
+
+	
+	return 0;
+}
